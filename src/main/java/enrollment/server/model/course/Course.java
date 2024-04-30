6a16e1c1a39e25d3a2e761e756c716d6dc676ce8
yeahjinjeong <yeahjin311@sookmyung.ac.kr>
2024-04-30 17:26:38 +0900

    feat(Course): add field - currentCapacity

--- src/main/java/enrollment/server/model/course/Course.java
+++ src/main/java/enrollment/server/model/course/Course.java
@@ -5,11 +5,11 @@ import enrollment.server.constants.Major;
 public class Course {
     // Class -> Course , 수업을 칭할 때 Course 사용이 보편적
     private final int id;
-    private int capacity;
+    private int capacity; // 증원가능성이 있으니 final로 하지 않았음
     private int credit; // 수업의 학점
     private String name;
     private String professor;
     private Prerequisite prerequisite; // 선행과목 - Course의 id 값들이 저장됨
     private Major major;
-
+    private int currentCapacity;
 }
