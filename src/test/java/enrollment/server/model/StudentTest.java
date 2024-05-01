a976e647d1391be7182f2c1a5fad4358a9339850
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-05-01 16:53:41 +0900

    test(StudentTest): create 선수과목 체크

--- src/test/java/enrollment/server/model/StudentTest.java
+++ src/test/java/enrollment/server/model/StudentTest.java
@@ -1,12 +1,18 @@
 package enrollment.server.model;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import enrollment.server.constants.Major;
 import enrollment.server.constants.Status;
+import enrollment.server.model.course.Course;
 import enrollment.server.model.course.EnrolledCourses;
+import enrollment.server.model.course.Prerequisite;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -20,5 +26,20 @@ public class StudentTest {
 
         // when, then
         assertThat(student.checkCurrentCredits(courseCredit)).isEqualTo(isPossible);
+    }
+
+    @DisplayName("선수과목 체크")
+    @Test
+    void checkPrerequisite() {
+        // given
+        Course java = new Course(101234,25, 3,"자바", "김동현", new Prerequisite(List.of()), Major.COMPUTER,0);
+        Course jpa = new Course(101111,25, 3,"스프링", "김동현", new Prerequisite(List.of(101234)), Major.COMPUTER,0);
+        Course spring = new Course(102222,25, 3,"스프링", "김동현", new Prerequisite(List.of(101111)), Major.COMPUTER,0);
+
+        Student student = new Student(1, 0, "변성일", new EnrolledCourses(Map.of("2023-1", List.of(java))), Major.COMPUTER, Status.ENROLLED);
+
+        // when, then
+        assertThat(checkPrerequisite(jpa)).isTrue();
+        assertThat(checkPrerequisite(spring)).isFalse();
     }
 }
