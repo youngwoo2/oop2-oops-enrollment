30dfa1a375973ae67632f85f0ac22b46857d12ae
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-05-01 16:52:38 +0900

    feat(Prerequisite): create Construtor

--- src/main/java/enrollment/server/model/course/Prerequisite.java
+++ src/main/java/enrollment/server/model/course/Prerequisite.java
@@ -5,4 +5,7 @@ import java.util.List;
 public class Prerequisite {
     private List<Integer> prerequisite; // 선행과목 - Course의 id 값들이 저장됨
 
+    public Prerequisite(List<Integer> prerequisite) {
+        this.prerequisite = prerequisite;
+    }
 }
