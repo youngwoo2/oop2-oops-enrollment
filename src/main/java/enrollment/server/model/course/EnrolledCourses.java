fcb7cb2f9d3e87c4c07cdc3ca744d187bd12ad5e
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-05-01 17:27:22 +0900

    feat(course): creaet Getter

--- src/main/java/enrollment/server/model/course/EnrolledCourses.java
+++ src/main/java/enrollment/server/model/course/EnrolledCourses.java
@@ -1,12 +1,17 @@
 package enrollment.server.model.course;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public class EnrolledCourses {
-    private Map<String, List<Course>> enrolledCourses; // 학생이 수강한 총 수업들, key : "2024-1", value :  2024년 1학기에 수강한 수업목록
+    private final Map<String, List<Course>> enrolledCourses; // 학생이 수강한 총 수업들, key : "2024-1", value :  2024년 1학기에 수강한 수업목록
 
     public EnrolledCourses(Map<String, List<Course>> enrolledCourses) {
         this.enrolledCourses = enrolledCourses;
+    }
+
+    public Map<String, List<Course>> getEnrolledCourses() {
+        return Collections.unmodifiableMap(enrolledCourses);
     }
 }
