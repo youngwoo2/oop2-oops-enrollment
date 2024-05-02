ded1fc6b2385f353beaf78e36166ccff4d2b603b
kimmame <kimmame@naver.com>
2024-05-02 14:41:32 +0900

    Feat: 클라이언트 TCP 수정

---
+++ src/test/java/clientTest/ClientTest.java
@@ -0,0 +1,13 @@
+package clientTest;
+
+import enrollment.client.controller.Controller;
+import org.junit.jupiter.api.Test;
+
+public class ClientTest {
+
+    @Test
+    void outputFormatTest(){
+        Controller controller = new Controller();
+
+    }
+}
